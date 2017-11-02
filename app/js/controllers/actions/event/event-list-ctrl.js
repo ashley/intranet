75a3cfcc6c47bae25057f051a8cb1002a2803040
ashley <msashleychen@gmail.com>
2017-11-02 01:24:26 -0400

    Refactor teamIdToName code

--- app/js/controllers/actions/event/event-list-ctrl.js
+++ app/js/controllers/actions/event/event-list-ctrl.js
@@ -2,7 +2,8 @@
 
 angular
 .module('app.controllers')
-.controller('EventListCtrl', function($scope, $sce, $rootScope, $stateParams, $state, Restangular, apiDescriptor, dataTransformer) {
+.controller('EventListCtrl', function($scope, $sce, $rootScope, $stateParams, $state,
+		Restangular, apiDescriptor, dataTransformer, preProcess) {
 	var resourceName = $stateParams.resourceName;
 	var resourceId = $stateParams.id;
 	$scope.resourceName = resourceName;
@@ -18,7 +19,7 @@ angular
 		return monthNames[month - 1] + " " + year;
 	}
 
-	var teamsIdToName = {};
+	var teamsIdToName = preProcess.teamIdtoNames();
 	var venuesIdToName = {};
 	var organizationIdToName = {};
 	var personIdToName = {};
